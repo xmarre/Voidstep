fe73cba3b284224dc002e22e1957ed025f2f0c2d
xmarre <54859656+xmarre@users.noreply.github.com>
2026-07-30 03:35:43 +0200

    Package CI artifacts as v1.1.1

--- scripts/build-ci.ps1
+++ scripts/build-ci.ps1
@@ -1,7 +1,7 @@
 param(
     [ValidateSet("Debug", "Release")]
     [string]$Configuration = "Release",
-    [string]$Version = "1.1.0"
+    [string]$Version = "1.1.1"
 )
 
 $ErrorActionPreference = "Stop"
